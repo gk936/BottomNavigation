1a27acc5c199a49cae1a44144eab3b290ade46c0
gk936 <gopikr936@gmail.com>
2022-08-10 15:25:26 +0530

    changes few logics in Health Fragments

--- app/src/main/java/com/example/bottomnavigation/fragments/ManageFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/fragments/ManageFragment.kt
@@ -14,7 +14,6 @@ class ManageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_manage, container, false)
     }
 
